b4259317dfcf01cf8872aec41c83d5c127aa2fc4
tarun028 <tarunsrivastavakvs@gmail.com>
2024-06-21 16:23:51 +0530

    continued

--- app/src/main/java/com/example/orderhere/StartActivity.kt
+++ app/src/main/java/com/example/orderhere/StartActivity.kt
@@ -1,11 +1,23 @@
 package com.example.orderhere
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        // Find the next button by its ID
+        val nextButton: Button = findViewById(R.id.nextBtn)
+
+        // Set OnClickListener to handle button clicks
+        nextButton.setOnClickListener {
+            // Create an Intent to navigate to the LoginActivity
+            val intent = Intent(this@StartActivity, loginScreen::class.java)
+            // Start the LoginActivity
+            startActivity(intent)
+        }
     }
 }
